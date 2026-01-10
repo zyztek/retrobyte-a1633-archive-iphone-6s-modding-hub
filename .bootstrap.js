b775775f344e78ac667bf9071d62e1fda24b49a7
Vibesdk <vibesdk-bot@cloudflare.com>
2026-01-10 15:21:42 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun install vitest"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
